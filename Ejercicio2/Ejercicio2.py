4cc7a7cacebe1f326c9ab3df8a5fe996077e7202
davidlopez0493 <davidlopez.93@hotmail.com>
2024-09-09 21:23:06 -0500

    Add files via upload

---
+++ Ejercicio2/Ejercicio2.py
@@ -0,0 +1,17 @@
+a, b = 7, 3,
+print(f"El resultado del punto A es: {a != b}")
+
+print(f"El resultado del punto B es: {a == b}")
+
+print(f"El resultado del punto C es: {a > b}")
+
+print(f"El resultado del punto D es: {(a+b) < 2}")
+
+print(f"El resultado del punto E es: {(a>7) and (b==3)}")
+
+print(f"El resultado del punto F es: {(a<7) or (b==3)}")
+
+print(f"El resultado del punto G es: {(a<7) or (b!=3)}")
+
+
+
